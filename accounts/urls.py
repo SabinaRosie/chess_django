2ee76c1935f3e9e3396b55f258043510e32ae068
SabinaRosie <sabinaniraula56@gmail.com>
2026-04-17 11:20:44 +0545

    Remove trailing slashes from API routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import signup, login
 
 urlpatterns = [
-    path('signup/', signup),
-    path('login/', login),
+    path('signup', signup),
+    path('login', login),
 ]
