272732ebe71d379a0c197786904872522efd7fa6
SabinaRosie <sabinaniraula56@gmail.com>
2026-05-14 13:15:03 +0545

    fixx: Unit and integration testing final

--- scratch/test_verify.py
+++ scratch/test_verify.py
@@ -7,13 +7,16 @@ sys.path.append(os.getcwd())
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sabina_chess.settings')
 django.setup()
 
+import pytest
 from accounts.views import verify_otp
 from accounts.models import OTPVerification, User
 
+@pytest.mark.django_db
 def test_verify_repro():
     email = "niraulasabina08@gmail.com"
-    user = User.objects.get(email=email, username__startswith='repro_user')
-    otp_rec = OTPVerification.objects.get(user=user)
+    # Create test data since pytest uses an empty database
+    user = User.objects.create_user(username='repro_user_99', email=email, password='password123')
+    otp_rec = OTPVerification.objects.create(user=user, otp='123456')
     otp = otp_rec.otp
     
     print(f"Attempting to verify {email} with OTP {otp}...")
