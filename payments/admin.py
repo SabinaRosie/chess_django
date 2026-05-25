3d944cfbb9592098864bc7dd9d87075186dc16b5
SabinaRosie <sabinaniraula56@gmail.com>
2026-05-25 16:15:48 +0545

    feat: payment history on admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product_id', 'amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('user__username', 'transaction_uuid', 'esewa_ref_id')
